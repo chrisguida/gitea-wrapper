7da0fdfa8b62b94d7ebac16f308efa2664c20d1d
Chris Guida <chrisguida@gmail.com>
2022-08-12 17:33:11 -0500

    fix config key names

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -20,8 +20,8 @@ SECRET_KEY=$(cat /data/start9/secret-key.txt)
 export GITEA__server__DOMAIN=$DOMAIN
 export GITEA__server__ROOT_URL="$PROTOCOL://$DOMAIN/"
 export GITEA__server__SSH_DOMAIN=$DOMAIN
-export GITEA__server__INSTALL_LOCK=true
-export GITEA__server__SECRET_KEY=$SECRET_KEY
+export GITEA__security__INSTALL_LOCK=true
+export GITEA__security__SECRET_KEY=$SECRET_KEY
 
 if [ "$(yq ".disable-registration" /data/start9/config.yaml)" = "true" ]; then
     export GITEA__service__DISABLE_REGISTRATION=true
