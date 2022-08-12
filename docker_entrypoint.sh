ce05a25e0250f428dea1a26c83cfa7a2113aad75
Matt Hill <MattDHill@users.noreply.github.com>
2022-08-12 16:51:43 -0600

    Hide install (#2)
    * secret key and install lock
    
    * start9 dir
    
    * fix typo
    
    * fix config key names
    
    Co-authored-by: Chris Guida <chrisguida@gmail.com>

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -11,9 +11,17 @@ else
     DOMAIN=$TOR_ADDRESS
     PROTOCOL=http
 fi
+
+if ! [ -f /data/start9/secret-key.txt ]; then
+    cat /dev/urandom | head -c 32 | base64 > /data/start9/secret-key.txt
+fi
+SECRET_KEY=$(cat /data/start9/secret-key.txt)
+
 export GITEA__server__DOMAIN=$DOMAIN
 export GITEA__server__ROOT_URL="$PROTOCOL://$DOMAIN/"
 export GITEA__server__SSH_DOMAIN=$DOMAIN
+export GITEA__security__INSTALL_LOCK=true
+export GITEA__security__SECRET_KEY=$SECRET_KEY
 
 if [ "$(yq ".disable-registration" /data/start9/config.yaml)" = "true" ]; then
     export GITEA__service__DISABLE_REGISTRATION=true
