23c4fda17d7afe20ebe5dd3216e7cdc19bff9f41
Matt Hill <matthewonthemoon@gmail.com>
2022-08-25 06:25:41 -0600

    failed health check message

--- check-user-signups-off.sh
+++ check-user-signups-off.sh
@@ -5,6 +5,6 @@ set -e
 if [ "$(yq e ".disable-registration" /data/start9/config.yaml)" = "true" ]; then
     exit 0
 else
-    echo "User signups should be disabled when not in use" >&2
+    echo "For security purposes, user account registration should be disabled when not in use. You can disable registration in Config settings." >&2
     exit 1
 fi
